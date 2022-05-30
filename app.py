2da7ac24aed5d1897a834e191b138c87cde23db1
sfenster <scottfenstermaker@gmail.com>
2022-05-30 13:56:08 -0500

    troubleshooting

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def respond():
 @app.route('/post/', methods=['POST'])
 def post_something():
     param = request.form.get('name')
-    print("Post parameter is: ", request.form.['name'])
+    print("Post parameter is: ", request.values.get('name'))
     print(param)
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
