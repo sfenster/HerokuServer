f5bf0f1798befadf676d9241463c50e388917bf2
sfenster <scottfenstermaker@gmail.com>
2022-05-30 11:50:42 -0500

    Add config support

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask,request,json
+import os
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route('/')
 def hello():
-    return 'Webhooks with Python - on HEROKU!'
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
 
 @app.route('/githubIssue',methods=['POST'])
 def githubIssue():
