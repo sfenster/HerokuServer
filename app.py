e19cd4025068f675a5323de0a5853746783722ea
sfenster <scottfenstermaker@gmail.com>
2022-06-11 08:49:53 -0500

    Modified files on Sat Jun 11 08:49:53 CDT 2022

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask,request,json,jsonify
 import os
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
+
+q = Queue(connection=conn)
+result = q.enqueue(count_words_at_url, 'http://heroku.com')
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
