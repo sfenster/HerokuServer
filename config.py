4271d2d4f060a8aafd8546ecadf29938544f9ab3
sfenster <scottfenstermaker@gmail.com>
2022-06-15 15:38:06 -0500

    Modified files on Wed Jun 15 15:38:06 CDT 2022

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEVELOPMENT = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 class StagingConfig(Config):
     DEVELOPMENT = True
