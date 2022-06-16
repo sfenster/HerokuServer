03a154c321491ae319ec51e41e2d1ee98d1f9c36
sfenster <scottfenstermaker@gmail.com>
2022-06-16 14:45:51 -0500

    Modified files on Thu Jun 16 14:45:51 CDT 2022

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 
 
 q = Queue(connection=conn)
-result = q.enqueue(count_words_at_url, 'http://heroku.com')
+#result = q.enqueue(count_words_at_url, 'http://heroku.com')
 
 
 app = Flask(__name__)
@@ -26,53 +26,66 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from models import *
 
-from models import Result
+def count_and_save_words(url):
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
     if request.method == "POST":
+        # this import solves a rq bug which currently exists
+        from app import count_and_save_words
+
         # get url that the person has entered
-        try:
-            url = request.form['url']
-            r = requests.get(url)
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-            return render_template('index.html', errors=errors)
-        if r:
-            # text processing
-            raw = BeautifulSoup(r.text, 'html.parser').get_text()
-            nltk.data.path.append('./nltk_data/')  # set the path
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
-            # remove punctuation, count raw words
-            nonPunct = re.compile('.*[A-Za-z].*')
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_word_count = Counter(raw_words)
-            # stop words
-            no_stop_words = [w for w in raw_words if w.lower() not in stops]
-            no_stop_words_count = Counter(no_stop_words)
-            # save the results
-            results = sorted(
-                no_stop_words_count.items(),
-                key=operator.itemgetter(1),
-                reverse=True
-            )
-            try:
-                result = Result(
-                    url=url,
-                    result_all=raw_word_count,
-                    result_no_stop_words=no_stop_words_count
-                )
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
+        url = request.form['url']
+        if not url[:8].startswith(('https://', 'http://')):
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
 
 
 
@@ -133,6 +146,7 @@ def post_something():
         return jsonify({
             "ERROR": "no name found, please send a name."
         })
+
 @app.route('/redis/', methods=['GET', 'POST'])
 def testredis():
         return render_template('index.html')
@@ -162,15 +176,13 @@ def get_results(job_key):
     job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
-        #result = Result.query.filter_by(id=job.result).first()
-        #results = sorted(
-        #    result.result_no_stop_words.items(),
-        #    key=operator.itemgetter(1),
-        #    reverse=True
-        #)[:10]
-        #return jsonify(results)
-        print("Job fetch result: " + str(job.result))
-        return str(job.result)
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
     else:
         return "Nay!", 202
 
