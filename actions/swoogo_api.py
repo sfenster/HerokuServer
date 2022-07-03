71fa1b4115a1d72793d915b69ac3d987019a5d05
sfenster <scottfenstermaker@gmail.com>
2022-07-03 08:07:44 -0500

    Modified files on Sun Jul  3 08:07:44 CDT 2022

--- actions/swoogo_api.py
+++ actions/swoogo_api.py
@@ -1,12 +1,16 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 
 def add_registrant(action_data, webhook_data):
     auth_url = "https://www.swoogo.com/api/v1/oauth2/token.json"
-    b64_key_secret = "UlR3TTJ5SlRDTjFCaWg5bmxLcEhoOnY3NDJkWVk5d1BKbGVrU2d2VlJZSmFQYTBoWGNsdkdpZnZCcElBREdKcA=="
+    b64_key_secret = os.getenv("swoogo_b64_key_secret")
     auth_headers = {'Content-Type':'application/x-www-form-urlencoded', 'Authorization':'Basic ' + b64_key_secret}
     auth_data = {'grant_type':'client_credentials'}
+
 
     auth_resp = requests.post(url=auth_url, headers=auth_headers, data=auth_data).json()
     #resp_dict = auth_resp.json()
