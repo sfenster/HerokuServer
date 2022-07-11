e29408f1cde9790ac23365d962fec5bc3a4c0d1e
sfenster <scottfenstermaker@gmail.com>
2022-07-11 08:17:39 -0500

    Modified files on Mon Jul 11 08:17:39 CDT 2022

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import re
 import nltk
 from datetime import datetime, timedelta
 import time
-from flask import Flask,request,json,jsonify,render_template
+from flask import Flask,request,json,jsonify,render_template,flash,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import redis
@@ -17,6 +17,8 @@ from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
 from pathlib import Path
+from flask_login import current_user,login_user,LoginManager,logout_user,login_required
+
 
 FILE_DIR = Path(__file__).resolve().parent
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
@@ -30,9 +32,11 @@ app.config.from_object(env_config)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from models import *
-
+from forms import *
 
 def run_workflow(name, webhook_data):
     #Get path of json workflow file matching webhook path, load file, get actions, call enqueue_action() to run
@@ -142,15 +146,107 @@ def get_registrants():
 
 #=========WORDCOUNT ENDPOINTS=========
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    #user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+@app.route('/wordcount', methods=['GET', 'POST'])
+@login_required
+def wordcount():
+    return render_template('wordcount.html')
 
 
 @app.route('/start', methods=['POST'])
 def get_counts():
     # this import solves a rq bug which currently exists
-    from app import count_and_save_words
+    #from app import count_and_save_words
 
     # get url
     data = json.loads(request.data.decode())
@@ -257,12 +353,6 @@ def respond():
 
     # Return the response in json format
     return jsonify(response)
-
-
-
-
-
-
 
 
 
